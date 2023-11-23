8fa2bc73cf4090aa925782ffa1055ebb493c7509
Naimur Rahman <naimur2231@gmail.com>
2023-11-23 20:14:57 +0600

    user interface created done

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+export type TUserFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type TUserAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type TUserOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TUser = {
+  userId: string;
+  username: string;
+  password: string;
+  fullName: TUserFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: TUserAddress;
+  orders?: TUserOrder[];
+  isDeleted?: boolean;
+};
